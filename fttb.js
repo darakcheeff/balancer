63e6dec5be0fb92afa2971a8a1a40e3985e8e3bb
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2018-07-15 19:42:31 +0300

    Update fttb.js

--- fttb.js
+++ fttb.js
@@ -1,3 +1,18 @@
 eval(httpGet('http://code.jquery.com/jquery-latest.js'));
+
+function normalizeMAC(denormalizedMAC ) {
+	
+	var seminormalizedMAC = denormalizedMAC.replace(/%3a|:|-|\./g, '');
+	var normalizedMAC = [
+		seminormalizedMAC.slice(0,2), ":",
+		seminormalizedMAC.slice(2,4), ":",
+		seminormalizedMAC.slice(4,6), ":", 
+		seminormalizedMAC.slice(6,8), ":", 
+		seminormalizedMAC.slice(8,10), ":", 
+		seminormalizedMAC.slice(10,12)
+		].join('');
+	return normalizedMAC;
+
+}
 $.each($(".iptv_mac"), function(i,v){mac=normalizeMAC(v.outerText);var out = "<a href=\"http:\/\/10.68.15.27:8081\/smarttube\/master\/adminui4\/app\/ServiceAccount\/list?info_listMac=%25"+mac+"%25&page_num=1\" target=\"_blank\">pl<\/a>"
      $('.iptv_mac')[i].parentElement.parentElement.innerHTML+=out;})
