e61ababcdf1b9af0254e9fd504b01e01c718b5cf
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2019-08-28 16:59:43 +0300

    Create finest.js

---
+++ finest.js
@@ -0,0 +1,12 @@
+$.each($("span.state-icon-red"), function(i,v){
+  if((v.innerText=="Не готов - Поствызывное состояние")&&(v.parentElement.parentElement.children[2].innerText>"00:02:00"))  {
+console.log(v.parentElement.parentElement.children[0].innerText);
+ setTimeout(function() {
+  v.parentElement.parentElement.click();
+  setTimeout(function() {
+    $("#mrButtonId_label").click();
+  }, 1000);
+}, 1000);
+}
+})
+
