8591a219e217c95adf8122ffa62b7b4165879eca
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2019-10-02 13:20:20 +0300

    Update finest.js

--- finest.js
+++ finest.js
@@ -1,13 +1,13 @@
 function finestAutoEnable(){
-  $.each($("span.state-icon-red"), function(i,v){
+   $.each($("span.state-icon-red",frames['finesse_gadget_2'].document), function(i,v){
     if((v.innerText=="Не готов - Поствызывное состояние")&&(v.parentElement.parentElement.children[2].innerText>"00:02:00"))  {
-      console.log(v.parentElement.parentElement.children[0].innerText);
-      setTimeout(function() {
-        v.parentElement.parentElement.click();
-        setTimeout(function() {
-          $("#mrButtonId_label").click();
-        }, 1000);
-      }, 1000);
+        console.log(v.parentElement.parentElement.children[0].innerText);
+		console.log(v.parentElement.parentElement.parentElement);
+        v.parentElement.parentElement.parentElement.click();
+		$("span #mrButtonId", frames['finesse_gadget_2'].document)[0].parentElement.click();
+		console.log($("#mrButtonId", frames['finesse_gadget_2']));
+      
+     
     }
   })
 }
@@ -15,4 +15,4 @@ setTimeout(function() {
 $("div[id='teamRoster']",frames['finesse_gadget_2'].document).bind("DOMSubtreeModified",function(){
 	setTimeout(function() {finestAutoEnable(); }, 3000);
 	});
-}, 15000);
+}, 5000);
