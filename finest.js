24fcc8d566958c47a6e6d8e7b76da2ae1472f303
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2019-10-02 11:30:45 +0300

    Update finest.js

--- finest.js
+++ finest.js
@@ -1,12 +1,17 @@
-$.each($("span.state-icon-red"), function(i,v){
-  if((v.innerText=="Не готов - Поствызывное состояние")&&(v.parentElement.parentElement.children[2].innerText>"00:02:00"))  {
-console.log(v.parentElement.parentElement.children[0].innerText);
- setTimeout(function() {
-  v.parentElement.parentElement.click();
-  setTimeout(function() {
-    $("#mrButtonId_label").click();
-  }, 1000);
-}, 1000);
+function finestAutoEnable(){
+  $.each($("span.state-icon-red"), function(i,v){
+    if((v.innerText=="Не готов - Поствызывное состояние")&&(v.parentElement.parentElement.children[2].innerText>"00:02:00"))  {
+      console.log(v.parentElement.parentElement.children[0].innerText);
+      setTimeout(function() {
+        v.parentElement.parentElement.click();
+        setTimeout(function() {
+          $("#mrButtonId_label").click();
+        }, 1000);
+      }, 1000);
+    }
+  })
 }
-})
 
+$('body').bind("DOMSubtreeModified",function(){
+	setTimeout(function() {finestAutoEnable(); }, 3000);
+	});
