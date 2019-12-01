0d5dac642a1d5ad0d077904e546533899e7d88c8
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2019-12-01 18:31:07 +0300

    Update cal.js

--- cal.js
+++ cal.js
@@ -4,4 +4,12 @@ chrome.runtime.sendMessage({'openid':true}, function(response) {
 chrome.runtime.sendMessage(debug, function(response) {
   console.log(rr=response.sess);
   document.getElementById("usernameid").value=response.sess;
-});
+  setTimeout(function(){
+    $.get("http://10.68.100.60/cal/do.php?getUserNameBySession="+document.getElementById("usernameid").value, function(ee){
+      e=JSON.parse(ee);
+      document.getElementById("useridid").value=document.getElementById("usernameid").value;
+      document.getElementById("usernameid").value=e;
+      $("h2 center")[0].innerText=e;
+    }
+   )}, 2000)
+  });
