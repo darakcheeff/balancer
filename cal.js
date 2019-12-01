06f8338561fd711c5fd09e45f6fd42792feac303
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2019-12-01 17:19:17 +0300

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,1 +1,3 @@
-$("h2 center")[0].innerText=localStorage.getItem("username");
+chrome.runtime.sendMessage({'openid':true}, function(response) {
+  console.log(response);
+});//$("h2 center")[0].innerText=localStorage.getItem("username");
