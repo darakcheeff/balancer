d8dc930dbc392eace76da7a81060c54062eee800
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2020-02-11 16:15:15 +0300

    Update uicSMS2.js

--- uicSMS2.js
+++ uicSMS2.js
@@ -1,11 +1,11 @@
-$(".cuesHeaderLink").last()[0].outerHTML='<a onclick="SMS2Table();" class="cuesHeaderLink uicSMS2" style="color: white;">Перехуярить</a>';
+$(".cuesHeaderLink").last()[0].outerHTML='<a onclick="SMS2Table();" class="cuesHeaderLink uicSMS2" style="color: white;">Удалить Москву</a>';
 
 function SMS2Table(){
-if($(".cuesSelectedDrawerItem").text()=="СПС_День2"){
-window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<hr><div>New table</div><hr>";
-window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<table text-align='center'><thead><tr style='background-color: #fce4d6;'><th></th><th>Обработано</th><th>Потеряно</th><th>Отвечено за 30с</th><th>SL</th><th>LCR</th></tr></thead><tbody></tbody></table>";
+if($(".cuesSelectedDrawerItem").text()=="СПС_день"){
+window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<hr><div>New table</div><hr>";
+window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<table text-align='center'><thead><tr style='background-color: #fce4d6;'><th></th><th>Обработано</th><th>Потеряно</th><th>Отвечено за 30с</th><th>SL</th><th>LCR</th></tr></thead><tbody></tbody></table>";
 aa='';a1=0;a2=0;a3=0;
-$("#tbody tr", window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){
+$("#tbody tr", window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){
 a1+=parseInt(v.children[4].innerText);
 a2+=parseInt(v.children[6].innerText);
 a3+=parseInt(v.children[5].innerText);
@@ -16,16 +16,16 @@ v.children[6].innerText+'</td><td>'+
 (parseInt(parseInt(v.children[6].innerText)*100/parseInt(v.children[4].innerText))|| 0) +'%</td></tr>'})
 a4=parseInt(a3*100/(a1+a2));
 a5=parseInt(a2*100/a1);
-window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML=window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML.replace('<tbody></tbody>', '<tbody style="text-align: center;">'+aa+'<tr  style="background-color:  #fce4d6;"><td>Итого:</td><td>'+a1+'</td><td>'+a2+'</td><td>'+a3+'</td><td  class="slBotnetClassSbs2">'+a4+'%</td><td class="lcrBotnetClassSbs2">'+a5+'%</td></tr></tbody>').split("NaN").join("0");
-$(".slBotnetClassSbs2", window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>=80){$(v).css('background-color', 'lightgreen');}else{$(v).css('background-color', 'pink');}})
-$(".lcrBotnetClassSbs2", window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>0){$(v).css('background-color', 'pink');}else{$(v).css('background-color', 'lightgreen');}})
+window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML=window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML.replace('<tbody></tbody>', '<tbody style="text-align: center;">'+aa+'<tr  style="background-color:  #fce4d6;"><td>Итого:</td><td>'+a1+'</td><td>'+a2+'</td><td>'+a3+'</td><td  class="slBotnetClassSbs2">'+a4+'%</td><td class="lcrBotnetClassSbs2">'+a5+'%</td></tr></tbody>').split("NaN").join("0");
+$(".slBotnetClassSbs2", window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>=80){$(v).css('background-color', 'lightgreen');}else{$(v).css('background-color', 'pink');}})
+$(".lcrBotnetClassSbs2", window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>0){$(v).css('background-color', 'pink');}else{$(v).css('background-color', 'lightgreen');}})
 }}
 
-$(".uicSMS2").click(function(){if($(".cuesSelectedDrawerItem").text()=="СПС_День2"){
-window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<hr><div>New table</div><hr>";
-window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<table text-align='center'><thead><tr style='background-color: #fce4d6;'><th></th><th>Обработано</th><th>Потеряно</th><th>Отвечено за 30с</th><th>SL</th><th>LCR</th></tr></thead><tbody></tbody></table>";
+$(".uicSMS2").click(function(){if($(".cuesSelectedDrawerItem").text()=="СПС_день"){
+window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<hr><div>New table</div><hr>";
+window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML+="<table text-align='center'><thead><tr style='background-color: #fce4d6;'><th></th><th>Обработано</th><th>Потеряно</th><th>Отвечено за 30с</th><th>SL</th><th>LCR</th></tr></thead><tbody></tbody></table>";
 aa='';a1=0;a2=0;a3=0;
-$("#tbody tr", window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){
+$("#tbody tr", window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){
 a1+=parseInt(v.children[4].innerText);
 a2+=parseInt(v.children[6].innerText);
 a3+=parseInt(v.children[5].innerText);
@@ -36,7 +36,7 @@ v.children[6].innerText+'</td><td>'+
 (parseInt(parseInt(v.children[6].innerText)*100/parseInt(v.children[4].innerText))|| 0) +'%</td></tr>'})
 a4=parseInt(a3*100/(a1+a2));
 a5=parseInt(a2*100/a1);
-window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML=window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML.replace('<tbody></tbody>', '<tbody style="text-align: center;">'+aa+'<tr  style="background-color:  #fce4d6;"><td>Итого:</td><td>'+a1+'</td><td>'+a2+'</td><td>'+a3+'</td><td  class="slBotnetClassSbs2">'+a4+'%</td><td class="lcrBotnetClassSbs2">'+a5+'%</td></tr></tbody>').split("NaN").join("0");
-$(".slBotnetClassSbs2", window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>=80){$(v).css('background-color', 'lightgreen');}else{$(v).css('background-color', 'pink');}})
-$(".lcrBotnetClassSbs2", window.frames['СПС_День2'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>0){$(v).css('background-color', 'pink');}else{$(v).css('background-color', 'lightgreen');}})
+window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML=window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer").innerHTML.replace('<tbody></tbody>', '<tbody style="text-align: center;">'+aa+'<tr  style="background-color:  #fce4d6;"><td>Итого:</td><td>'+a1+'</td><td>'+a2+'</td><td>'+a3+'</td><td  class="slBotnetClassSbs2">'+a4+'%</td><td class="lcrBotnetClassSbs2">'+a5+'%</td></tr></tbody>').split("NaN").join("0");
+$(".slBotnetClassSbs2", window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>=80){$(v).css('background-color', 'lightgreen');}else{$(v).css('background-color', 'pink');}})
+$(".lcrBotnetClassSbs2", window.frames['СПС_день'].contentWindow.frames['view1_iframe'].frames['viewframe'].contentDocument.getElementById("scrollTableContainer")).each(function(i,v){if(parseInt(v.innerText)>0){$(v).css('background-color', 'pink');}else{$(v).css('background-color', 'lightgreen');}})
 }})
